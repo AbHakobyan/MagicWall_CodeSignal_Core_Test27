61a9b108ca7c01572656bf475a09ec24be5a3b01
Albert Hakobyan <abhakobyan24@gmail.com>
2021-08-28 10:20:29 +0400

    Добавьте файлы проекта.

---
+++ MagicWall_CodeSignal_Core_Test27/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicWall_CodeSignal_Core_Test27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = magicalWell(5, 6, 4);
+            Console.WriteLine(n);
+        }
+
+
+        static int magicalWell(int a, int b, int n)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++, a++, b++)
+            {
+                count += a * b;
+            }
+            return count;
+        }
+    }
+}
